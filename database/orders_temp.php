2070698affbee4fe1ab6bef557fa8c5438a1962c
Onasi Cuevas <onasic96@gmail.com>
2019-03-16 21:50:30 -0400

    Styling the product list

--- database/orders_temp.php
+++ database/orders_temp.php
@@ -1,2 +1,2 @@
 <?php exit; ?>
-a:2:{i:1552619505407;a:1:{i:1;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:5:"79.00";s:5:"sName";s:6:"Argent";}}i:1552757982197;a:1:{i:2;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:5:"99.00";s:5:"sName";s:12:"Myobelt 5 SB";}}}
+a:3:{i:1552619505407;a:1:{i:1;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:5:"79.00";s:5:"sName";s:6:"Argent";}}i:1552757982197;a:1:{i:2;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:5:"99.00";s:5:"sName";s:12:"Myobelt 5 SB";}}i:1552783524200;a:1:{i:1;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:5:"79.00";s:5:"sName";s:6:"Argent";}}}
